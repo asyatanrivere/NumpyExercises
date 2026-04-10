2fc690a858970205c1ae3ac3570641c65e973516
Elif Asya Tanrıvere <asyatanrivere@gmail.com>
2026-04-10 18:26:47 +0300

    datatype completed

--- datatype.py
+++ datatype.py
@@ -28,3 +28,40 @@ print(arri) # [ 2  5 32]
 print(arri.dtype) # int 32
 print(arri8) # [ 2  5 32]
 print(arri8.dtype) # int64
+
+arrf4 = np.array([2.543,97.35434],dtype= "f4")
+arrf8 = np.array([2.5443533,97.3235255434],dtype= "f8")
+print(arrf4) # [ 2.543   97.35434]
+print(arrf4.dtype) # float32
+print(arrf8) # [ 2.5443533  97.32352554]
+print(arrf8.dtype) # float64
+
+arru1=np.array([100,200], dtype="u1") # unsigned bit is 8 bit and it's between 0 and 255
+                                      # 256 is too big
+                                      # just positive numbers
+arru4=np.array([100,800], dtype="u4")
+print(arru1) # [100 200]
+print(arru1.dtype) # uint8
+print(arru4) # [100 800]
+print(arru4.dtype) # uint32
+
+arrs=np.array([b"hello",b"python"], dtype="S") # byte string -> ASCII
+                                               # byte string -> byte string (ü,ö,ş,i etc are unvalid)
+                                               # string -> UNICODE (str)                                             
+print(arrs) # [b'hello' b'python']
+print(arrs.dtype) # |S6
+arrs5=np.array([b"hello",b"python"], dtype="S5") # max 5 character
+print(arrs5) # [b'hello' b'pytho']
+print(arrs5.dtype) # |S5
+
+arrb=np.array([True,False], dtype="b")
+print(arrb) # [1 0]
+print(arrb.dtype) # int8
+
+arro=np.array([2,43.2375,"Asya",[2,53,2],True], dtype="object")
+print(arro) # [2 43.2375 'Asya' list([2, 53, 2]) True]
+print(arro.dtype) # object
+
+arrdate=np.array(["2004-11-18","1938-10-29"], dtype="M8[D]")
+print(arrdate) # ['2004-11-18' '1938-10-29']
+print(arrdate.dtype) # datetime64[D]
